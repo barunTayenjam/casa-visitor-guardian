0b0c213b31be00f56cb532e23a350e61ea853dd8
Barun <barun.2009.tam@gmail.com>
2025-07-10 20:37:58 +0530

    fix: update core application files

--- server/src/types/global.d.ts
+++ server/src/types/global.d.ts
@@ -1,13 +1,16 @@
 import { StreamManager } from '../streams/rtspManager';
 import { SimpleMotionDetection } from '../detection/simpleMotionDetection';
+import { PersonDetector } from '../detection/personDetection';
 
 declare global {
   var streamManager: StreamManager;
   var motionDetector: SimpleMotionDetection;
+  var personDetector: PersonDetector;
   namespace NodeJS {
     interface Global {
       streamManager: StreamManager;
       motionDetector: SimpleMotionDetection;
+      personDetector: PersonDetector;
     }
   }
 }
